b9d142a91bfd385b0382926fd14645f0594bccad
Dwakesh <brqtsix521@WIN-B34IQUDRKT4>
2024-02-20 06:07:30 -0800

    implimented routes and working on login page

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginPageComponent } from '../../Pages/login-page/login-page.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,LoginPageComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   options: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-
+  
+  sendTForm :any;
   // Variable to store the selected option
   selectedOption: any = '';
 
 
   onSelect() {
     // this.selectedOption = option;
+  }
+  constructor(private router: Router) {}
+
+  goToPage()
+  {
+    this.router.navigate(['/LoginPage']);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { LoginPageComponent} from './Pages/login-page/login-page.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    { path: '', component: HomePageComponent },
+    { path: 'LoginPage', component: LoginPageComponent }
+
+];
